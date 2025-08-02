464065151d2811e394bda7769c492ebe0a685b57
Esteban Abanto <esteban.abanto.2709@gmail.com>
2025-08-02 14:39:18 -0500

    Fix(Server): Update CORS configuration to allow all origins in development

--- server/src/server.ts
+++ server/src/server.ts
@@ -25,7 +25,7 @@ const io = new Server<
     SocketData
 >(httpServer, {
     cors: {
-        origin: ['http://localhost:5173'], // Development client URL
+        origin: process.env.NODE_ENV === 'production' ? false : '*',
         methods: ['GET', 'POST']
     },
     pingTimeout: 60000,
